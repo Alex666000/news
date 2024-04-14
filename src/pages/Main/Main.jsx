b4df9eb51b3f9e413ef2a62fdd6ace760b3461b2
Alex666000 <bogdanov777000@mail.ru>
2024-04-14 17:41:50 +0300

    add Skeleton

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,31 +1,35 @@
-import { useEffect, useState } from "react";
-import NewsBanner from "../../components/NewsBanner/NewsBanner";
-import styles from "./styles.module.css";
-import { getNews } from "../../api/apiNews";
-import NewsList from "../../components/NewsList/NewsList";
+import {useEffect, useState} from 'react';
+import NewsBanner from '../../components/NewsBanner/NewsBanner';
+import styles from './styles.module.css';
+import {getNews} from '../../api/apiNews';
+import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton/Skeleton.jsx'
 
 const Main = () => {
-  const [news, setNews] = useState([]);
+    const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await getNews();
-        setNews(response.news);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchNews();
-  }, []);
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                setIsLoading(true)
+                const response = await getNews();
+                setNews(response.news);
+                setIsLoading(false)
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchNews();
+    }, []);
 
-  return (
-    <main className={styles.main}>
-      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+    return (
+        <main className={styles.main}>
+            {news.length > 0 && !isLoading ? <NewsBanner item={news[0]}/> : <Skeleton count={1} type={'banner'}/>}
+            {!isLoading ? <NewsList news={news}/> : <Skeleton count={10} type={'item'}/>}
 
-      <NewsList news={news} />
-    </main>
-  );
+        </main>
+    );
 };
 
 export default Main;

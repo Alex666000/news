58cd63a4c073231c012feff91f9177624c4ded58
Alex666000 <bogdanov777000@mail.ru>
2024-04-15 19:09:02 +0300

    add NewsPage

--- src/features/pagination/model/types.ts
+++
@@ -1,7 +0,0 @@
-export interface IPaginationProps {
-  totalPages: number;
-  handlePreviousPage: () => void;
-  handleNextPage: () => void;
-  handlePageClick: (page: number) => void;
-  currentPage: number;
-}
--- src/shared/helpers/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (date: Date) => {
-  const options: Intl.DateTimeFormatOptions = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  return date.toLocaleDateString("en-US", options);
-};
